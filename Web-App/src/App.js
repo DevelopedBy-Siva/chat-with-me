7e5dbbc085892dde4bd26d228e02a127615597a2
Sivasanker <developedby.siva@gmail.com>
2022-07-23 22:18:04 +0530

    Refactor the Web-App

--- Web-App/src/App.js
+++ Web-App/src/App.js
@@ -1,5 +1,5 @@
-import Container from "./Container";
-import GlobalStyles from "./GlobalStyles";
+import Container from "./Components/Container";
+import GlobalStyles from "./Styles/GlobalStyles";
 
 function App() {
   return (
