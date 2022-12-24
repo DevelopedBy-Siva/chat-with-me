aa744f222385b3dd7815bcc66cf9ac3c8090c764
DevelopedBy-Siva <developedby.siva@gmail.com>
2022-12-24 16:14:22 +0530

    Client Side: Fix active nav

--- client/src/components/Home/NavBar.js
+++ client/src/components/Home/NavBar.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { IoIosSettings } from "react-icons/io";
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdSupervisorAccount } from "react-icons/md";
@@ -36,22 +36,11 @@ const navBtns = [
 ];
 
 export default function NavBarContainer() {
-  const [activeNav, setActiveNav] = useState(0);
-
-  function handleNavigation(index) {
-    setActiveNav(index);
-  }
-
   return (
     <Container>
       {navBtns.map((nav, index) => (
         <React.Fragment key={index}>
-          <NavBtn
-            onClick={() => handleNavigation(index)}
-            active={index === activeNav}
-            to={nav.navTo}
-            id={nav.id}
-          >
+          <NavBtn to={nav.navTo} id={nav.id}>
             {nav.icon}
           </NavBtn>
           <Tooltip id={nav.id} msg={nav.placeholder} />
@@ -70,7 +59,7 @@ const Container = styled.nav`
   flex-direction: column;
 `;
 
-const NavBtn = styled(Link)`
+const NavBtn = styled(NavLink)`
   width: 100%;
   height: 60px;
   display: flex;
@@ -78,27 +67,21 @@ const NavBtn = styled(Link)`
   align-items: center;
   margin-bottom: 2px;
   cursor: pointer;
-  color: ${(props) => props.theme.text.main};
-  background: ${(props) =>
-    props.active ? props.theme.background.highlight.rgba : "none"};
-  background-image: ${(props) =>
-    props.active
-      ? `linear-gradient(to right, ${props.theme.background.highlight.rgba}, ${props.theme.background.container})`
-      : "none"};
   outline: none;
-  border: ${(props) =>
-    `1px solid ${
-      props.active
-        ? props.theme.background.highlight.rgba
-        : props.theme.background.container
-    }`};
+  color: ${(props) => props.theme.text.main};
   border-right: none;
-  border-left: ${(props) =>
-    `4px solid ${
-      props.active
-        ? props.theme.background.highlight.hex
-        : props.theme.background.container
-    }`};
+  border: ${(props) => `1px solid ${props.theme.background.container}`};
+  border-left: ${(props) => `4px solid ${props.theme.background.container}`};
+
+  &.active {
+    background: ${(props) => props.theme.background.highlight.rgba};
+    background-image: ${(props) =>
+      `linear-gradient(to right, ${props.theme.background.highlight.rgba}, ${props.theme.background.container})`};
+    border: ${(props) => `1px solid ${props.theme.background.highlight.rgba}`};
+    border-left: ${(props) =>
+      `4px solid ${props.theme.background.highlight.hex}`};
+  }
+
   .iconStyle {
     font-size: 90%;
   }
