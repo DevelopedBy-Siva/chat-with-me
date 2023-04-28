e426fb98c617eefd1013f42fed80b66c8ec033c8
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-04-29 01:54:41 +0530

    Add timeout to socket.io

--- client/src/context/SocketContext.js
+++ client/src/context/SocketContext.js
@@ -3,19 +3,21 @@ import { v4 as uuid } from "uuid";
 import io from "socket.io-client";
 
 const baseURL = process.env.REACT_APP_API_BASEURL;
+const socketTimeout = process.env.REACT_APP_API_TIMEOUT;
 
 const SocketContext = React.createContext();
 
-export function useSocket() {
-  return useContext(SocketContext);
-}
+export const useSocket = () => useContext(SocketContext);
 
 export function SocketProvider({ id, token, children }) {
   const [socket, setSocket] = useState();
 
   useEffect(() => {
     const uniqueId = id + "--__--" + uuid();
-    const newSocket = io(baseURL, { query: { id: uniqueId, token } });
+    const newSocket = io(baseURL, {
+      query: { id: uniqueId, token },
+      timeout: socketTimeout,
+    });
     setSocket(newSocket);
 
     return () => newSocket.close();
