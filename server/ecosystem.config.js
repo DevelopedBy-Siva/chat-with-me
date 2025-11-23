80458da6a7bdd947f9b189e971d9cd4bc02a4b53
DevelopedBy-Siva <developedby-siva@gmail.com>
2025-11-23 05:12:56 -0700

    load env as part of pm2

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: "./index.js",
       instances: 1,
       exec_mode: "fork",
+      env_file: ".env",
       env: {
         NODE_ENV: "production",
         PORT: 5000,
